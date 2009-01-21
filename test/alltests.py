a98aaf04c94abc97c0cee80adbee0b8b67140542
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2009-01-21 08:13:17 +0000

    TJ/TS: removed unnecessary comment.
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/trunk@1274 83e7a582-4515-4195-8f39-222cbda6297e

--- test/alltests.py
+++ test/alltests.py
@@ -74,8 +74,7 @@ from oai.oaitooltest import OaiToolTest
 from oai.oaiprovenancetest import OaiProvenanceTest
 from oai.resumptiontokentest import ResumptionTokenTest
 from oai.oaisetselecttest import OaiSetSelectTest
-from oai.xml2documenttest import Xml2DocumentTest  # --> Xml2Document only in use by OAI; deprecated; needs to be replaced
-                                               #     when OAI is worked on next.
+from oai.xml2documenttest import Xml2DocumentTest
 from oai.oaijazzfiletest import OaiJazzFileTest
 from oai.oaiaddrecordtest import OaiAddRecordTest
 from oai.oaijazztest import OaiJazzWithFileTest, OaiJazzWithLuceneTest
