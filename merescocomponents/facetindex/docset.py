ac70263386275e5cce282134a1f273aafa614492
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2009-10-05 14:56:31 +0000

    TJ: improved message on add wrong docIds to a DocSet.
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/workingsets/2.21.4-TUD/trunk@1817 83e7a582-4515-4195-8f39-222cbda6297e

--- merescocomponents/facetindex/docset.py
+++ merescocomponents/facetindex/docset.py
@@ -149,8 +149,9 @@ class DocSet(object):
 
     def add(self, doc):
         l = DocSet_len(self)
-        if l > 0 and doc <= DocSet_get(self, l-1):
-            raise Exception('non-increasing docid: %d must be > %d' % (doc, l))
+        lastDoc = DocSet_get(self, l-1)
+        if l > 0 and doc <= lastDoc:
+            raise Exception('non-increasing docid: %d must be > %d, (length %d)' % (doc, lastDoc, l))
         DocSet_add(self._cobj, doc)
 
     def merge(self, anotherDocSet):
