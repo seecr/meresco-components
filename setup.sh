607a209945108e8c115c95fd9e989a0d5eb37ce9
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2010-08-12 12:55:20 +0000

    TJ: copied and edit setup scripts.
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/tags/version_3.0.2@2559 83e7a582-4515-4195-8f39-222cbda6297e

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-MACHINE=`gcc -dumpmachine`
-GCCVERSION=`gcc -dumpversion`
-GCCVERSION_SHORT=$(echo $GCCVERSION | awk -F. '{print $1"."$2}')
-
-LIBRARY_PATH=/usr/lib/gcc/$MACHINE/${GCCVERSION} \
-CPLUS_INCLUDE_PATH=/usr/include/c++/${GCCVERSION}:/usr/lib/gcc/$MACHINE/${GCCVERSION}/include \
-CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} python2.5 setup.py build_ext --inplace
+if [ -f /etc/debian_version ]; then
+    ./debian.setup.sh "$@"
+elif [ -f /etc/redhat-release ]; then
+    ./redhat.setup.sh "$@"
+else
+    echo "Unsupported platform"
+fi
