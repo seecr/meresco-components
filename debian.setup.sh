8c6cf47ecfee4adc4d49d703c00b0b8bd22e25b5
hendrikmoree <hendrikmoree@83e7a582-4515-4195-8f39-222cbda6297e>
2011-10-11 12:12:17 +0000

    JJ/HM: Setup.sh to python2.6
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/workingsets/4.0-beta1-Seecr/version_3@3866 83e7a582-4515-4195-8f39-222cbda6297e

--- debian.setup.sh
+++ debian.setup.sh
@@ -5,4 +5,4 @@ GCCVERSION_SHORT=$(echo $GCCVERSION | awk -F. '{print $1"."$2}')
 
 LIBRARY_PATH=/usr/lib/gcc/$MACHINE/${GCCVERSION} \
 CPLUS_INCLUDE_PATH=/usr/include/c++/${GCCVERSION}:/usr/lib/gcc/$MACHINE/${GCCVERSION}/include \
-CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} python2.5 setup.py build_ext --inplace
+CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} python2.6 setup.py build_ext --inplace
--- testsetup.sh
+++ testsetup.sh
@@ -8,7 +8,7 @@ GCCVERSION_SHORT=$(echo $GCCVERSION | awk -F. '{print $1"."$2}')
 
 LIBRARY_PATH=/usr/lib/gcc/$MACHINE/${GCCVERSION} \
 CPLUS_INCLUDE_PATH=/usr/include/c++/${GCCVERSION}:/usr/lib/gcc/$MACHINE/${GCCVERSION}/include \
-CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} python2.5 setup.py install --root tmp
+CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} python2.6 setup.py install --root tmp
 
 cp meresco/__init__.py tmp/usr/lib/python2.5/site-packages/meresco
 export PYTHONPATH=`pwd`/tmp/usr/lib/python2.5/site-packages
