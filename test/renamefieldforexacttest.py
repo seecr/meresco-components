5e422ee7db1890dbba1d6e36130654936dfcff88
johan_jonkers <johan_jonkers@83e7a582-4515-4195-8f39-222cbda6297e>
2011-11-11 13:07:27 +0000

    JJ: applied license
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/workingsets/4.0-beta1-Seecr/version_6@3946 83e7a582-4515-4195-8f39-222cbda6297e

--- test/renamefieldforexacttest.py
+++ test/renamefieldforexacttest.py
@@ -1,28 +1,26 @@
 ## begin license ##
 # 
-# "Edurep" is a service for searching in educational repositories.
-# "Edurep" is developed for Stichting Kennisnet (http://www.kennisnet.nl) by
-# Seek You Too (http://www.cq2.nl). The project is based on the opensource
-# project Meresco (http://www.meresco.com). 
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core". 
 # 
 # Copyright (C) 2010-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
 # Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
 # 
-# This file is part of "Edurep"
+# This file is part of "Meresco Components"
 # 
-# "Edurep" is free software; you can redistribute it and/or modify
+# "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # 
-# "Edurep" is distributed in the hope that it will be useful,
+# "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with "Edurep"; if not, write to the Free Software
+# along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 ## end license ##
