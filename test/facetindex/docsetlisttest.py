f4e0ecb06bd958126de17ef71a80f277bc3d8007
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2012-01-05 12:49:47 +0000

    HM/TJ: docsetlist improved by removing unnecessary sorting. Renamed _TEST_getDocsetForTerm to getDocsetForTerm.
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/workingsets/3.4.26.1-HBO/version_0-hierarchical_drilldown_speedup@4135 83e7a582-4515-4195-8f39-222cbda6297e

--- test/facetindex/docsetlisttest.py
+++ test/facetindex/docsetlisttest.py
@@ -1,32 +1,34 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-#
-#    Meresco Components are components to build searchengines, repositories
-#    and archives, based on Meresco Core.
-#    Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
-#    Copyright (C) 2007-2009 Stichting Kennisnet Ict op school.
-#       http://www.kennisnetictopschool.nl
-#    Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
-#    Copyright (C) 2009 Tilburg University http://www.uvt.nl
-#    Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
-#
-#    This file is part of Meresco Components.
-#
-#    Meresco Components is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    Meresco Components is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Components; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
+# 
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core". 
+# 
+# Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
+# Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
+# Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
+# Copyright (C) 2009 Tilburg University http://www.uvt.nl
+# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "Meresco Components"
+# 
+# "Meresco Components" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "Meresco Components" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Components"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
 ## end license ##
+
 from time import time
 from meresco.components.facetindex import DocSetList, DocSet
 from meresco.components.facetindex.docsetlist import JACCARD_ONLY, JACCARD_MI
@@ -161,8 +163,8 @@ class DocSetListTest(LuceneTestCase):
         #self.assertEquals([3], intersections[0])
         self.assertEquals([3, 4], intersections[1])
         self.assertEquals(2, len(intersections))
-        self.assertEquals([3], intersections._TEST_getDocsetForTerm('t0'))
-        self.assertEquals([3, 4], intersections._TEST_getDocsetForTerm('t2'))
+        self.assertEquals([3], intersections.getDocsetForTerm('t0'))
+        self.assertEquals([3, 4], intersections.getDocsetForTerm('t2'))
 
     def testTermIntersect(self):
         docsetlist0 = DocSetList()
@@ -176,8 +178,8 @@ class DocSetListTest(LuceneTestCase):
         docsetlist1.add(DocSet([8]), 'not_in_0')
 
         docsetlistResult = docsetlist0.termIntersect(docsetlist1)
-        self.assertEquals([0, 1, 2], docsetlistResult._TEST_getDocsetForTerm('t1'))
-        self.assertEquals([0, 1   ], docsetlistResult._TEST_getDocsetForTerm('t0'))
+        self.assertEquals([0, 1, 2], docsetlistResult.getDocsetForTerm('t1'))
+        self.assertEquals([0, 1   ], docsetlistResult.getDocsetForTerm('t0'))
         self.assertEquals(2, len(docsetlistResult))
 
     def testTermIntersectZipperReliesOnSearch(self):
@@ -190,8 +192,8 @@ class DocSetListTest(LuceneTestCase):
         docsetlist1.add(DocSet([0]), 't99')
 
         docsetlistResult = docsetlist0.termIntersect(docsetlist1)
-        self.assertEquals([0], docsetlistResult._TEST_getDocsetForTerm('t0'))
-        self.assertEquals([0], docsetlistResult._TEST_getDocsetForTerm('t99'))
+        self.assertEquals([0], docsetlistResult.getDocsetForTerm('t0'))
+        self.assertEquals([0], docsetlistResult.getDocsetForTerm('t99'))
         self.assertEquals(2, len(docsetlistResult))
 
     def testReadTermsFrom_SEGMENT_reader(self):
@@ -584,13 +586,13 @@ class DocSetListTest(LuceneTestCase):
         dsl = DocSetList()
         dsl.add(ds0, 'term0')
         dsl.add(ds1, 'term1')
-        ds0new = dsl._TEST_getDocsetForTerm('term0')
-        ds1new = dsl._TEST_getDocsetForTerm('term1')
+        ds0new = dsl.getDocsetForTerm('term0')
+        ds1new = dsl.getDocsetForTerm('term1')
         self.assertEquals(ds0, ds0new)
         self.assertEquals(ds1, ds1new)
         dsl.sortOnCardinality()
-        ds0new = dsl._TEST_getDocsetForTerm('term0')
-        ds1new = dsl._TEST_getDocsetForTerm('term1')
+        ds0new = dsl.getDocsetForTerm('term0')
+        ds1new = dsl.getDocsetForTerm('term1')
         self.assertEquals(ds0, ds0new)
         self.assertEquals(ds1, ds1new)
 
