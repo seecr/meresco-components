81ac048377250df2211d439e5f63ba09e23d8bc0
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2012-01-17 09:59:26 +0000

    TS/TJ: testsetup.sh improved.
    git-svn-id: file:///home/ozp/probeer-tj/meresco/meresco-components/workingsets/4.0-beta2-Seecr/version_0-new-weightless@4160 83e7a582-4515-4195-8f39-222cbda6297e

--- testsetup.sh
+++ testsetup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-set -e
+## begin license ##
+# 
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core". 
+# 
+# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "Meresco Components"
+# 
+# "Meresco Components" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "Meresco Components" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Components"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+## end license ##
+
+set -o errexit
 
 rm -rf tmp build
 fullPythonVersion=python2.6
@@ -11,14 +36,22 @@ LIBRARY_PATH=/usr/lib/gcc/$MACHINE/${GCCVERSION} \
 CPLUS_INCLUDE_PATH=/usr/include/c++/${GCCVERSION}:/usr/lib/gcc/$MACHINE/${GCCVERSION}/include \
 CPP=cpp-${GCCVERSION_SHORT} CC=gcc-${GCCVERSION_SHORT} CXX=g++-${GCCVERSION_SHORT} ${fullPythonVersion} setup.py install --root tmp
 
+VERSION="x.y.z"
+
+find tmp -name '*.py' -exec sed -r -e \
+    "/DO_NOT_DISTRIBUTE/ d;
+    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/" -i '{}' \;
+
 cp meresco/__init__.py tmp/usr/local/lib/${fullPythonVersion}/dist-packages/meresco
-export PYTHONPATH=`pwd`/tmp/usr/local/lib/${fullPythonVersion}/dist-packages
+export PYTHONPATH=`pwd`/tmp/usr/local/lib/${fullPythonVersion}/dist-packages:${PYTHONPATH}
 cp -r test tmp/test
 
+set +o errexit
 (
 cd tmp/test
 ./alltests.sh
 )
+set -o errexit
 
 rm -rf tmp build
 
