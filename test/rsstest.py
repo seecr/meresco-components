bafadec8d94a73e0d355189cb97fff7b6e22857f
Seecr Development Team <development@seecr.nl>
2013-05-10 09:33:32 +0200

    TJ: amara removed from imports and test.

--- test/rsstest.py
+++ test/rsstest.py
@@ -1,36 +1,37 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2007-2009 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 # Copyright (C) 2007-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2011 Stichting Kennisnet http://www.kennisnet.nl
-# 
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from seecr.test import SeecrTestCase, CallTrace
-from amara.binderytools import bind_string
 from urllib import urlencode
+from StringIO import StringIO
+from lxml.etree import parse
 
 from meresco.components.facetindex import Response
 from meresco.components.rss import Rss
@@ -119,9 +120,9 @@ class RssTest(SeecrTestCase):
         rss.addObserver(observer)
         result = "".join(compose(rss.handleRequest(RequestURI='/?query=aQuery%29'))) #%29 == ')'
 
-        xml = bind_string(result[result.index("<?xml"):])
-        self.assertEquals('Test title', str(xml.rss.channel.title))
-        self.assertFalse('''An error occurred 'Unexpected token after parsing''' in str(xml.rss.channel.description), str(xml.rss.channel.description))
+        xml = parse(StringIO(result[result.index("<?xml"):]))
+        self.assertEquals(['Test title'], xml.xpath('/rss/channel/title/text()'))
+        self.assertEquals(['Test description'], xml.xpath('/rss/channel/description/text()'))
 
     def testErrorNoQuery(self):
         rss = Rss(
@@ -131,9 +132,9 @@ class RssTest(SeecrTestCase):
         )
         result = "".join(compose(rss.handleRequest(RequestURI='/')))
 
-        xml = bind_string(result[result.index("<?xml"):])
-        self.assertEquals('ERROR Test title', str(xml.rss.channel.title))
-        self.assertTrue('''An error occurred 'MANDATORY parameter 'query' not supplied or empty''' in str(xml.rss.channel.description), str(xml.rss.channel.description))
+        xml = parse(StringIO(result[result.index("<?xml"):]))
+        self.assertEquals(['ERROR Test title'], xml.xpath('/rss/channel/title/text()'))
+        self.assertEquals(["An error occurred 'MANDATORY parameter 'query' not supplied or empty'"], xml.xpath('/rss/channel/description/text()'))
 
 
     def assertMaxAndSort(self, maximumRecords, sortKey, sortDirection, rssArgs, sruArgs):
