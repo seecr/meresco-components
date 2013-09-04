05b503abe58f077f94f735b71322a65481a30e89
Seecr Development Team <development@seecr.nl>
2013-09-04 09:41:11 +0200

    HM: Added httpspost and httpsget and httppost to HttpClient

--- meresco/components/http/httpclient.py
+++ meresco/components/http/httpclient.py
@@ -1,29 +1,29 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# and archives, based on "Meresco Core".
+#
+# Copyright (C) 2012-2013 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-from weightless.http import httpget
+from weightless.http import httpget, httppost#, httpspost, httpsget
 from meresco.components.http.utils import CRLF
 from lxml.etree import parse as lxmlParse
 from StringIO import StringIO
@@ -32,7 +32,26 @@ from urllib import urlencode
 class HttpClient(object):
 
     def httpGet(self, hostname, port, path, arguments, parse=True, **kwargs):
-        response = yield httpget(hostname, port, '%s?%s' % (path, urlencode(arguments)))
-        headers, body = response.split(CRLF*2)
-        raise StopIteration((headers, lxmlParse(StringIO(body)) if parse else body))
+        raise StopIteration((
+            yield _doRequest(httpget, parse=parse, host=hostname, port=port, request='%s?%s' % (path, urlencode(arguments)))
+        ))
 
+    def httpsGet(self, hostname, port, path, arguments, parse=True, **kwargs):
+        raise StopIteration((
+            yield _doRequest(httpsget, parse=parse, host=hostname, port=port, request='%s?%s' % (path, urlencode(arguments)))
+        ))
+
+    def httpPost(self, hostname, port, path, data, parse=True, **kwargs):
+        raise StopIteration((
+            yield _doRequest(httppost, parse=parse, host=hostname, port=port, path=path, body=data)
+        ))
+
+    def httpsPost(self, hostname, port, path, data, parse=True, **kwargs):
+        raise StopIteration((
+            yield _doRequest(httpspost, parse=parse, host=hostname, port=port, path=path, body=data)
+        ))
+
+def _doRequest(method, parse, **kwargs):
+    response = yield method(**kwargs)
+    headers, body = response.split(CRLF*2)
+    raise StopIteration((headers, lxmlParse(StringIO(body)) if parse else body))
--- test/http/httpclienttest.py
+++ test/http/httpclienttest.py
@@ -1,64 +1,87 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# and archives, based on "Meresco Core".
+#
+# Copyright (C) 2012-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://stichting.bibliotheek.nl
-# 
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from seecr.test import SeecrTestCase
 
 from meresco.components.http.utils import CRLF
+from meresco.components.http import httpclient
 from meresco.components.http.httpclient import HttpClient
-
-from weightless.core import compose
 
 from meresco.components import lxmltostring
 
+from seecr.utils.generatorutils import generatorReturn, returnValueFromGenerator
+
 class HttpClientTest(SeecrTestCase):
 
-    def sendAndReceive(self, gen, answer):
-        shouldBeGenerator = str(gen.next())
-        self.assertTrue(shouldBeGenerator.startswith("<generator object httpget at 0x"), shouldBeGenerator)
-       
-        try:
-            gen.send(answer)
-        except StopIteration, e:
-            headers, body =  e.args[0]
-        return headers, body
+    def setUp(self):
+        SeecrTestCase.setUp(self)
+        def httpRequest(**kwargs):
+            generatorReturn(self.response)
+            yield
+        httpclient.httpget = httpRequest
+        httpclient.httppost = httpRequest
+        httpclient.httpsget = httpRequest
+        httpclient.httpspost = httpRequest
 
     def testPlainText(self):
         client = HttpClient()
+        self.response = """HTTP/1.0 200 Ok\r\nContent-Type: text/xml\r\n\r\n<xml/>"""
 
         gen = client.httpGet(hostname='localhost', port=80, path='/', arguments={}, parse=False)
-        headers, body = self.sendAndReceive(gen, """HTTP/1.0 200 Ok\r\nContent-Type: text/xml\r\n\r\n<xml/>""")
+        headers, body = returnValueFromGenerator(gen)
 
         self.assertEquals('<xml/>', body)
         self.assertEquals(['HTTP/1.0 200 Ok', 'Content-Type: text/xml'], headers.split(CRLF))
-    
+
     def testPlainXml(self):
         client = HttpClient()
+        self.response = """HTTP/1.0 200 Ok\r\nContent-Type: text/xml\r\n\r\n<xml/>"""
 
         gen = client.httpGet(hostname='localhost', port=80, path='/', arguments={})
-        headers, body = self.sendAndReceive(gen,  """HTTP/1.0 200 Ok\r\nContent-Type: text/xml\r\n\r\n<xml/>""")
-        
+        headers, body = returnValueFromGenerator(gen)
+
         self.assertEquals('<xml/>', lxmltostring(body))
         self.assertEquals(['HTTP/1.0 200 Ok', 'Content-Type: text/xml'], headers.split(CRLF))
+
+    def testHttpPost(self):
+        client = HttpClient()
+        self.response = """HTTP/1.0 200 Ok\r\n\r\nother-data"""
+
+        gen = client.httpPost(hostname='localhost', port=80, path='/', data='data', parse=False)
+        headers, body = returnValueFromGenerator(gen)
+
+        self.assertEquals('other-data', body)
+        self.assertEquals(['HTTP/1.0 200 Ok'], headers.split(CRLF))
+
+    def testHttpsGet(self):
+        client = HttpClient()
+        self.response = """HTTP/1.0 200 Ok\r\n\r\nother-data"""
+
+        gen = client.httpsGet(hostname='localhost', port=443, path='/', arguments={}, parse=False)
+        headers, body = returnValueFromGenerator(gen)
+
+        self.assertEquals('other-data', body)
+        self.assertEquals(['HTTP/1.0 200 Ok'], headers.split(CRLF))
