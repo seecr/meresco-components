7feae6e55ee509766903f2f9d5a73ab671b980ae
Seecr Development Team <development@seecr.nl>
2013-09-10 10:10:45 +0200

    HM/TJ: Ok -> OK.

--- meresco/components/http/utils.py
+++ meresco/components/http/utils.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2007-2009 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
@@ -11,23 +11,23 @@
 # Copyright (C) 2010 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2010 Stichting Kennisnet http://www.kennisnet.nl
 # Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from weightless.core import Yield
@@ -39,22 +39,22 @@ ContentTypeHtml = "text/html; charset=utf-8"
 ContentTypePlainText = "text/plain; charset=utf-8"
 ContentTypeHeader = "Content-Type: "
 
-Ok = "HTTP/1.0 200 Ok" + CRLF
+Ok = "HTTP/1.0 200 OK" + CRLF
 
 #200
-okXml = "HTTP/1.0 200 OK" + CRLF + \
+okXml = Ok + \
         ContentTypeHeader + ContentTypeXml + CRLF + \
         CRLF
 
-okRss = "HTTP/1.0 200 OK" + CRLF + \
+okRss = Ok + \
         ContentTypeHeader + ContentTypeRss + CRLF + \
         CRLF
 
-okHtml = "HTTP/1.0 200 OK" + CRLF + \
+okHtml = Ok + \
         ContentTypeHeader + ContentTypeHtml + CRLF + \
         CRLF
 
-okPlainText = "HTTP/1.0 200 OK" + CRLF + \
+okPlainText = Ok + \
         ContentTypeHeader + ContentTypePlainText + CRLF + \
         CRLF
 
@@ -99,7 +99,7 @@ serverErrorHtml = "HTTP/1.0 500 Internal Server Error" + CRLF +\
 #503
 serverUnavailableHtml = "HTTP/1.0 503 Service Unavailable" + CRLF +\
                         ContentTypeHeader + ContentTypeHtml + CRLF +\
-                        CRLF 
+                        CRLF
 
 def insertHeader(httpResponse, extraHeader):
     alreadyDone = False
