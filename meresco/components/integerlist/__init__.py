f7d7a22a09fb0657a3c2ed9cf9f3c38e55c769c1
Seecr Development Team <development@seecr.nl>
2013-11-21 11:47:59 +0100

    TJ: run on python2.7 only if 2.6 not available.

--- meresco/components/integerlist/__init__.py
+++ meresco/components/integerlist/__init__.py
@@ -34,7 +34,9 @@ mydir = abspath(dirname(__file__))                           #DO_NOT_DISTRIBUTE
 projectdir = dirname(dirname(dirname(mydir)))                #DO_NOT_DISTRIBUTE
 if isdir(join(projectdir, '.git')):                          #DO_NOT_DISTRIBUTE
     from os import system                                    #DO_NOT_DISTRIBUTE
-    status = system("cd %s; python2.6 setup.py build_ext --inplace"  % projectdir)  #DO_NOT_DISTRIBUTE
+    from sys import version_info as v                        #DO_NOT_DISTRIBUTE
+    pythonExe = "python%s.%s" % (v.major, v.minor)           #DO_NOT_DISTRIBUTE
+    status = system("cd %s; %s setup.py build_ext --inplace"  % (projectdir, pythonExe))  #DO_NOT_DISTRIBUTE
     if status > 0:                                           #DO_NOT_DISTRIBUTE
         import sys                                           #DO_NOT_DISTRIBUTE
         sys.exit(status)                                     #DO_NOT_DISTRIBUTE
