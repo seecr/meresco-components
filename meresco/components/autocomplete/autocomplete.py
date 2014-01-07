f8bd8b7302cb6dae36c6e79e04405f58d67d32f7
Thijs Janssen <thijs@seecr.nl>
2014-01-07 14:54:57 +0100

    TJ: type=text/xml.

--- meresco/components/autocomplete/autocomplete.py
+++ meresco/components/autocomplete/autocomplete.py
@@ -1,31 +1,30 @@
 ## begin license ##
-# 
-# "NBC+" also known as "ZP (ZoekPlatform)" is
-#  initiated by Stichting Bibliotheek.nl to provide a new search service
-#  for all public libraries in the Netherlands. 
-# 
+#
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2009-2011 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009-2011 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# Copyright (C) 2011 Stichting Kennisnet http://www.kennisnet.nl
+# Copyright (C) 2011-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011, 2014 Stichting Kennisnet http://www.kennisnet.nl
 # Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
-# 
-# This file is part of "NBC+ (Zoekplatform BNL)"
-# 
-# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
+#
+# This file is part of "Meresco Components"
+#
+# "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
+#
+# "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
+# along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from xml.sax.saxutils import escape as escapeXml
@@ -33,7 +32,6 @@ from meresco.core import Observable
 from meresco.components.http.utils import Ok as HttpOk, ContentTypeHeader, CRLF, okXml
 from meresco.components.http import FileServer
 from simplejson import dumps as dumps_json
-from urllib import quote
 from os.path import join, dirname, abspath
 
 filesDir = join(dirname(abspath(__file__)), 'files')
@@ -69,9 +67,6 @@ class Autocomplete(Observable):
         fieldname = arguments.get('field', [self._defaultField])[0]
         limit = int(arguments.get('limit', [self._defaultLimit])[0])
         
-        terms = []
-        descriptions = []
-
         yield HttpOk
         yield ContentTypeHeader + ContentTypeJsonSuggestions + CRLF
         yield "Access-Control-Allow-Origin: *" + CRLF
@@ -89,7 +84,7 @@ class Autocomplete(Observable):
         yield """<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
     <ShortName>%(shortname)s</ShortName>
     <Description>%(description)s</Description>
-    <Url type="text/html" method="get" template="http://%(host)s:%(port)s%(temlateQuery)s"/>
+    <Url type="text/xml" method="get" template="http://%(host)s:%(port)s%(temlateQuery)s"/>
     <Url type="application/x-suggestions+json" template="http://%(host)s:%(port)s%(path)s?prefix={searchTerms}"/>
 </OpenSearchDescription>""" % {
             'shortname': self._shortname,
