23f2d9911958b73266783a02782aa62794eaf9dd
Seecr Development Team <development@seecr.nl>
2014-02-17 10:01:25 +0100

    TS: added updateIps to Deproxy component.
    Delegates to its IpFilter.

--- meresco/components/http/deproxy.py
+++ meresco/components/http/deproxy.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2010-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from meresco.core import Observable
@@ -46,6 +47,10 @@ class Deproxy(Observable):
                 Headers['Host'] = host
                 port = host.partition(':')[2] or '80'
         yield self.all.handleRequest(Client=(clientHost, clientPort), Headers=Headers, port=port, **kwargs)
+
+    def updateIps(self, ipAddresses=None, ipRanges=None):
+        self._ipfilter.updateIps(ipAddresses=ipAddresses, ipRanges=ipRanges)
+
 
 def _firstFromCommaSeparated(s):
     return s.split(",", 1)[0].strip()
--- test/http/deproxytest.py
+++ test/http/deproxytest.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2010-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from unittest import TestCase
@@ -32,88 +33,72 @@ from meresco.core import Observable
 
 from seecr.test import CallTrace
 
+from meresco.core import Observable
+
 from meresco.components.http import Deproxy
 
-from weightless.core import compose
+from weightless.core import compose, be, consume
 
-
-def handleRequest(*args, **kwargs):
-    return
-    yield
 
 class DeproxyTest(TestCase):
+    def setUp(self):
+        TestCase.setUp(self)
+
+        self.createTree(deproxyForIps=['1.1.1.1'])
+
+    def createTree(self, **kwargs):
+        self.deproxy = Deproxy(**kwargs)
+        self.observer = CallTrace('Observer', emptyGeneratorMethods=['handleRequest'])
+        self.top = be((Observable(),
+            (self.deproxy,
+                (self.observer,),
+            )
+        ))
+
     def testClientInCaseNoXForwardedForHeader(self):
-        clientfromxforwardedfor = Deproxy(deproxyForIps=['1.1.1.1'])
-        observer = CallTrace(methods={'handleRequest': handleRequest})
-        clientfromxforwardedfor.addObserver(observer)
+        list(compose(self.top.all.handleRequest(Client=("1.1.1.1", 11111), Headers={})))
 
-        observable = Observable()
-        observable.addObserver(clientfromxforwardedfor)
-
-        list(compose(observable.all.handleRequest(Client=("1.1.1.1", 11111), Headers={})))
-
-        self.assertEquals(1, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[0].kwargs
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
         self.assertEquals("1.1.1.1", handleRequestCallKwargs['Client'][0])
         self.assertEquals({}, handleRequestCallKwargs['Headers'])
 
-
     def testDeproxy(self):
-        clientfromxforwardedfor = Deproxy(deproxyForIps=['1.1.1.1'])
-        observer = CallTrace(methods={'handleRequest': handleRequest})
-        clientfromxforwardedfor.addObserver(observer)
-
-        observable = Observable()
-        observable.addObserver(clientfromxforwardedfor)
-
-        list(compose(observable.all.handleRequest(
+        list(compose(self.top.all.handleRequest(
             Client=("1.1.1.1", 11111),
             Headers={"X-Forwarded-For": "2.2.2.2"})))
 
-        self.assertEquals(1, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[0].kwargs
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
         self.assertEquals("2.2.2.2", handleRequestCallKwargs['Client'][0])
         self.assertEquals({"X-Forwarded-For": "2.2.2.2"}, handleRequestCallKwargs['Headers'])
 
     def testClientFromMulitpleXForwardedForEntries(self):
-        clientfromxforwardedfor = Deproxy(deproxyForIps=['1.1.1.1'])
-        observer = CallTrace(methods={'handleRequest': handleRequest})
-        clientfromxforwardedfor.addObserver(observer)
-
-        observable = Observable()
-        observable.addObserver(clientfromxforwardedfor)
-
-        list(compose(observable.all.handleRequest(
+        list(compose(self.top.all.handleRequest(
              Client=("1.1.1.1", 11111),
              Headers={"X-Forwarded-For": "2.2.2.2,3.3.3.3,4.4.4.4"})))
 
-        self.assertEquals(1, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[0].kwargs
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
         self.assertEquals("2.2.2.2", handleRequestCallKwargs['Client'][0])
         self.assertEquals({"X-Forwarded-For": "2.2.2.2,3.3.3.3,4.4.4.4"}, handleRequestCallKwargs['Headers'])
 
-        list(compose(observable.all.handleRequest(
+        list(compose(self.top.all.handleRequest(
              Client=("1.1.1.1", 11111),
              Headers={"X-Forwarded-For": " 2.2.2.2 , 3.3.3.3,4.4.4.4"})))
-        self.assertEquals("2.2.2.2", observer.calledMethods[1].kwargs['Client'][0])
+        self.assertEquals("2.2.2.2", self.observer.calledMethods[1].kwargs['Client'][0])
 
     def testHostFromXForwardedHost(self):
-        clientfromxforwardedfor = Deproxy(deproxyForIpRanges=[
+        self.createTree(deproxyForIpRanges=[
             ('9.9.9.0', '9.9.9.255')])
-        observer = CallTrace(methods={'handleRequest': handleRequest})
-        clientfromxforwardedfor.addObserver(observer)
-
-        observable = Observable()
-        observable.addObserver(clientfromxforwardedfor)
-
         Headers={
             "Host": "1.1.1.1:11111",
             "X-Forwarded-Host": "2.2.2.2:22222,3.3.3.3:33333,4.4.4.4:44444"
         }
-        list(compose(observable.all.handleRequest(Client=("9.9.9.9", 9999), port='11111', Headers=Headers)))
+        consume(self.top.all.handleRequest(Client=("9.9.9.9", 9999), port='11111', Headers=Headers))
 
-        self.assertEquals(1, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[0].kwargs
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
         self.assertEquals("2.2.2.2:22222", handleRequestCallKwargs['Headers']['Host'])
         self.assertEquals("22222", handleRequestCallKwargs['port'])
 
@@ -121,10 +106,10 @@ class DeproxyTest(TestCase):
             "Host": "1.1.1.1:11111",
             "X-Forwarded-Host": "2.2.2.2,3.3.3.3:33333,4.4.4.4:44444"
         }
-        list(compose(observable.all.handleRequest(Client=("9.9.9.9", 9999), port='11111', Headers=Headers)))
+        consume(self.top.all.handleRequest(Client=("9.9.9.9", 9999), port='11111', Headers=Headers))
 
-        self.assertEquals(2, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[1].kwargs
+        self.assertEquals(2, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[1].kwargs
         self.assertEquals("2.2.2.2", handleRequestCallKwargs['Headers']['Host'])
         self.assertEquals("80", handleRequestCallKwargs['port'])
 
@@ -132,19 +117,41 @@ class DeproxyTest(TestCase):
         self.assertRaises(ValueError, Deproxy)
 
     def testDeproxyForIps(self):
-        clientfromxforwardedfor = Deproxy(deproxyForIps=['3.3.3.3'])
-        observer = CallTrace(methods={'handleRequest': handleRequest})
-        clientfromxforwardedfor.addObserver(observer)
-
-        observable = Observable()
-        observable.addObserver(clientfromxforwardedfor)
-
-        list(compose(observable.all.handleRequest(
+        self.createTree(deproxyForIps=['3.3.3.3'])
+        consume(self.top.all.handleRequest(
             Client=("1.1.1.1", 11111),
-            Headers={"X-Forwarded-For": "2.2.2.2"})))
+            Headers={"X-Forwarded-For": "2.2.2.2"}))
 
-        self.assertEquals(1, len(observer.calledMethods))
-        handleRequestCallKwargs = observer.calledMethods[0].kwargs
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
         self.assertEquals("1.1.1.1", handleRequestCallKwargs['Client'][0])
         self.assertEquals({"X-Forwarded-For": "2.2.2.2"}, handleRequestCallKwargs['Headers'])
+
+    def testDeproxyUpdateIps(self):
+        # Expose updateIps from IpFilter
+        self.createTree(deproxyForIps=['127.7.7.7'])
+
+        # White box
+        allowDeproxying = lambda ip: self.deproxy._ipfilter.filterIpAddress(ip)
+        self.assertEquals(True, allowDeproxying('127.7.7.7'))
+        self.assertEquals(False, allowDeproxying('127.0.0.1'))
+        self.assertEquals(False, allowDeproxying('10.0.0.1'))
+
+        self.deproxy.updateIps(ipAddresses='192.168.96.96', ipRanges=[('10.0.0.0', '10.0.0.2')])
+        self.assertEquals(True, allowDeproxying('192.168.96.96'))
+        self.assertEquals(True, allowDeproxying('10.0.0.1'))
+        self.assertEquals(False, allowDeproxying('127.7.7.7'))
+        self.assertEquals(False, allowDeproxying('127.0.0.1'))
+
+        # Black box
+        consume(self.top.all.handleRequest(
+            Client=("192.168.96.96", 12345),
+            Headers={
+                "X-Forwarded-For": "2.2.2.2",
+                "X-Forwarded-Host": "example.org"}))
+
+        self.assertEquals(1, len(self.observer.calledMethods))
+        handleRequestCallKwargs = self.observer.calledMethods[0].kwargs
+        self.assertEquals("2.2.2.2", handleRequestCallKwargs['Client'][0])
+        self.assertEquals({"X-Forwarded-For": "2.2.2.2", "X-Forwarded-Host": "example.org", "Host": "example.org"}, handleRequestCallKwargs['Headers'])
 
