f92cf781772e7c331b1c2b8607346e0dab43c3b3
Seecr Development Team <development@seecr.nl>
2014-03-06 11:45:27 +0100

    TS: added WEIGHTLESS_COMPOSE_TEST="PYTHON" / Weightless asserts-on flag to alltests.

--- test/alltests.sh
+++ test/alltests.sh
@@ -27,6 +27,7 @@
 
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
+export WEIGHTLESS_COMPOSE_TEST="PYTHON"
 pyversions=""
 if [ -e /usr/bin/python2.6 ]; then
     pyversions="python2.6"
