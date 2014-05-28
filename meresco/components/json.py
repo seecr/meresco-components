ce50247a6be4a20087d975fa102f07c477385dc7
Seecr Development Team <development@seecr.nl>
2014-05-28 14:04:44 +0200

    TJ: JsonDict and JsonList.

---
+++ meresco/components/json.py
@@ -0,0 +1,54 @@
+## begin license ##
+#
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core".
+#
+# Copyright (C) 2012-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2012-2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+# Copyright (C) 2014 Stichting Kennisnet http://www.kennisnet.nl
+#
+# This file is part of "Meresco Components"
+#
+# "Meresco Components" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Meresco Components" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Components"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from simplejson import dumps, dump, loads, load
+
+
+class _Json(object):
+    def dumps(self, *args, **kwargs):
+        return dumps(self, *args, **kwargs)
+    __str__ = dumps
+
+    def pretty_print(self, indent=4):
+        return dumps(self, indent=indent, sort_keys=True)
+
+    def dump(self, fp, *args, **kwargs):
+        return dump(self, fp, *args, **kwargs)
+
+    @classmethod
+    def loads(clz, s, *args, **kwargs):
+        return clz(loads(s, *args, **kwargs))
+
+    @classmethod
+    def load(clz, fp, *args, **kwargs):
+        return clz(load(fp, *args, **kwargs))
+
+class JsonDict(dict, _Json):
+    pass
+
+class JsonList(list, _Json):
+    pass
