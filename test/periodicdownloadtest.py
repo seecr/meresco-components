7b35a22712d5c78c6cd8276f452b815fd50409c6
Seecr Development Team <development@seecr.nl>
2014-06-04 16:21:47 +0200

    TS: tested not shutting down TCP socket connection "write half" too soon (whiteboxed).

--- test/periodicdownloadtest.py
+++ test/periodicdownloadtest.py
@@ -734,6 +734,42 @@ For request: GET /path?argument=value HTTP/1.0\r\n\r\n""" % repr(downloader) % f
         list(compose(processOne))
         self.assertEquals(['addReader', 'removeReader', 'addTimer'], reactor.calledMethodNames())
 
+    def testSocketNotShutdownForWriteAfterWriteDone(self):
+        # A.k.a don't send TCP FIN until HTTP Request finished please ;-)
+        client, server = socketpair(AF_UNIX)
+        client.setblocking(0)
+        server.setblocking(0)
+        reactor = CallTrace()
+        class BuildRequest(object):
+            def buildRequest(self, additionalHeaders=None):
+                # A.k.a. not fitting into the buffer.
+                return 'small request'
+        downloader = PeriodicDownload(reactor, host='localhost', port=9999, err=StringIO())
+        def mockTryConnect(host, port):
+            raise StopIteration(client)
+            yield
+        downloader._tryConnect = mockTryConnect
+        downloader._currentProcess = compose(downloader._processOne())
+        downloader.addObserver(BuildRequest())
+
+        downloader._currentProcess.next()
+        self.assertEquals(['addReader'], reactor.calledMethodNames())
+        sleep(0.01)
+        self.assertEquals('small request', server.recv(4096))
+
+        sleep(0.01)
+        try:
+            _ = server.recv(1)
+        except SocketError, (errno, msg):
+            self.assertEquals(EAGAIN, errno)
+        else:
+            self.fail('TCP connection must remain open until the request is finished.')
+
+        # cleanup
+        reactor.calledMethods.reset()
+        list(downloader._currentProcess)
+        self.assertEquals(['removeReader', 'addTimer'], reactor.calledMethodNames())
+
     def testShortRequestSendWithoutReactor(self):
         client, server = socketpair(AF_UNIX)
         client.setblocking(0)
