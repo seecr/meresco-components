de854a49909ab7ff553b672dec6e35715a208108
Thijs Janssen <thijs@seecr.nl>
2014-11-17 12:07:26 +0100

    JPM/TJ: FilterMessage now accepts name in __init__.

--- meresco/components/filtermessages.py
+++ meresco/components/filtermessages.py
@@ -1,30 +1,31 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2007-2009 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Netherlands Institute for Sound and Vision http://instituut.beeldengeluid.nl/
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from weightless.core import NoneOfTheObserversRespond, DeclineMessage
@@ -32,13 +33,14 @@ from meresco.core import Observable
 
 
 class FilterMessages(Observable):
-    def __init__(self, allowed=[], disallowed=[]):
-        Observable.__init__(self)
-        assert len(allowed) == 0 or len(disallowed) == 0, 'Use disallowed or allowed'
-        if allowed:
+    def __init__(self, allowed=None, disallowed=None, **kwargs):
+        Observable.__init__(self, **kwargs)
+        if allowed and not disallowed:
             self._allowedMessage = lambda message: message in allowed
-        else:
+        elif disallowed and not allowed:
             self._allowedMessage = lambda message: message not in disallowed
+        else:
+            raise ValueError('Use disallowed or allowed')
 
     def any_unknown(self, message, *args, **kwargs):
         if self._allowedMessage(message):
--- test/filtermessagestest.py
+++ test/filtermessagestest.py
@@ -1,26 +1,27 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# and archives, based on "Meresco Core".
+#
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Netherlands Institute for Sound and Vision http://instituut.beeldengeluid.nl/
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from seecr.test import SeecrTestCase, CallTrace
@@ -34,20 +35,20 @@ class FilterMessagesTest(SeecrTestCase):
     def setUp(self):
         SeecrTestCase.setUp(self)
         self.observer1 = CallTrace(
-            'observer1', 
-            emptyGeneratorMethods=['message'], 
+            'observer1',
+            emptyGeneratorMethods=['message'],
             returnValues={
-                'function': 41, 
+                'function': 41,
                 'gen': (i for i in [41]),
                 'noop': None
             }
         )
         self.observer2 = object()
         self.observer3 = CallTrace(
-            'observer3', 
-            emptyGeneratorMethods=['message'], 
+            'observer3',
+            emptyGeneratorMethods=['message'],
             returnValues={
-                'function': 42, 
+                'function': 42,
                 'gen': (i for i in [42]),
                 'noop': None
             }
@@ -82,9 +83,4 @@ class FilterMessagesTest(SeecrTestCase):
         self.assertEquals([42], list(compose(self.dna.any.gen())))
 
     def testEitherAllowedOrDisallowed(self):
-        try:
-            fm = FilterMessages(allowed=['either'], disallowed=['or'])
-        except AssertionError, e:
-            self.assertEquals('Use disallowed or allowed', str(e))
-        else:
-            self.fail('Should not happen')
+        self.assertRaises(ValueError, lambda: FilterMessages(allowed=['either'], disallowed=['or']))
