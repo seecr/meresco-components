1056b619783424a34de83eb10fac3bdcb4f90301
Thijs Janssen <thijs@seecr.nl>
2015-01-15 13:49:47 +0100

    TJ: ParseArguments will show attribute types or defaults.

--- meresco/components/parsearguments.py
+++ meresco/components/parsearguments.py
@@ -1,28 +1,28 @@
 ## begin license ##
-# 
-# "NBC+" also known as "ZP (ZoekPlatform)" is
-#  initiated by Stichting Bibliotheek.nl to provide a new search service
-#  for all public libraries in the Netherlands. 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core".
+#
+# Copyright (C) 2011-2012, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
-# 
-# This file is part of "NBC+ (Zoekplatform BNL)"
-# 
-# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+#
+# This file is part of "Meresco Components"
+#
+# "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
+#
+# "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
+# along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from optparse import OptionParser, Option
@@ -47,6 +47,8 @@ class ParseArguments(object):
         if 'help' in kwargs and 'default' in kwargs and '{default}' in kwargs['help']:
             kwargs['help'] = kwargs['help'].format(default=kwargs['default'])
         option = Option(*args, **kwargs)
+        if option.metavar is None:
+            option.metavar = repr(option.default) if 'default' in kwargs else '<{0}>'.format(option.type)
         if mandatory:
             self._mandatoryKeys.append(option.dest)
         self._parser.add_option(option)
