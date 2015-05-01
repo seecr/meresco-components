e53f659ea3795af7971a27a035172903906de381
Seecr Development Team <development@seecr.nl>
2015-05-01 15:44:25 +0200

    HM: More generic autocomplete

--- meresco/components/autocomplete/files/jquery.autocomplete.js
+++ meresco/components/autocomplete/files/jquery.autocomplete.js
@@ -1,3 +1,29 @@
+/* begin license *
+ *
+ * "Meresco Components" are components to build searchengines, repositories
+ * and archives, based on "Meresco Core".
+ *
+ * Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+ * Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+ *
+ * This file is part of "Meresco Components"
+ *
+ * "Meresco Components" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * "Meresco Components" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Meresco Components"; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * end license */
+
 /**
  * Extending jQuery with autocomplete
  * Version: 1.4.2
@@ -149,7 +175,7 @@ $.fn.autocomplete = function(options){ return this.each(function(){
     }
     return true;
   });
-  
+
   me.keyup(function(ev){
     // ev.which doesn't work here - it always returns 0
     switch(ev.keyCode){
@@ -329,8 +355,12 @@ $.fn.autocomplete = function(options){ return this.each(function(){
       //var output = val.substring(0,st)+"<em>"+val.substring(st,st+len)+"</em>"+val.substring(st+len);
 
       var span = $('<span class="jqac-link">'+output+'</span>').get(0);
-      if (arr[i].info != undefined && arr[i].info != ""){
-        $(span).append($('<div class="jqac-info">'+arr[i].info+'</div>'));
+      var keys = Object.keys(arr[i]);
+      for (var j=0;j<keys.length;j++) {
+        var key = keys[j];
+        if (key != "id" && key != "value") {
+          $(span).append($('<div class="jqac-' + key + '">'+arr[i][key]+'</div>'));
+        }
       }
 
       $(span).attr('name',i+1);
