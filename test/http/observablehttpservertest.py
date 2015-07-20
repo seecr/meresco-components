a0e417b1625017ad255cc53435c3a81056cdb81b
Seecr Development Team <development@seecr.nl>
2015-07-20 10:53:11 +0200

    TS/TJ: rewrote test to show prio is added to the reactor

--- test/http/observablehttpservertest.py
+++ test/http/observablehttpservertest.py
@@ -1,37 +1,36 @@
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2007-2009 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 # Copyright (C) 2007-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2012, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://stichting.bibliotheek.nl
-# 
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-from socket import socket
 from seecr.test import SeecrTestCase, CallTrace
-from weightless.io import Reactor
 from weightless.core import compose
 
 from meresco.components.http import ObservableHttpServer
@@ -122,35 +121,11 @@ class ObservableHttpServerTest(SeecrTestCase):
         self.assertEquals(False, httpserver._compressResponse)
 
     def testServerWithPrio(self):
-        prios = []
-        class MyServer(object):
-            def handleRequest(self, *args, **kwargs):
-                yield 'HALLO'
-        class MyReactor(Reactor):
-            def addReader(self, *args, **kwargs):
-                prios.append(('read', kwargs['prio']))
-                return Reactor.addReader(self, *args, **kwargs)
-            def addWriter(self, *args, **kwargs):
-                prios.append(('write', kwargs['prio']))
-                return Reactor.addWriter(self, *args, **kwargs)
-        reactor = MyReactor()
+        reactor = CallTrace('reactor')
         s = ObservableHttpServer(reactor, 2000, prio=3)
-        s.addObserver(MyServer())
         s.observer_init()
-
-        sok = socket()
-        sok.connect(('localhost', 2000))
-        sok.send('GET / HTTP/1.0\r\n\r\n')
-        reactor.step()
-        reactor.step()
-        self.assertEquals([('read', 3)], prios)
-        reactor.step().step()
-        self.assertEquals([('read', 3), ('read', 3)], prios)
-        reactor.step()
-        reactor.step()
-        self.assertEquals([('read', 3), ('read', 3), ('write', 3)], prios)
-        # one more step to let the connection finalize and all objects reclaimed and avoid garbage
-        reactor.step()
+        self.assertEquals(['addReader'], reactor.calledMethodNames())
+        self.assertEquals(3, reactor.calledMethods[0].kwargs['prio'])
 
     def testServerBindAddress(self):
         reactor = CallTrace()
