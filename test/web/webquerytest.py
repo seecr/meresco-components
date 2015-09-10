61ffc47d713c5c03464781df6e4f9dbe424f4e2d
Seecr Development Team <development@seecr.nl>
2015-09-10 11:23:16 +0200

    HM/TJ: WebQuery.query returns a QueryExpression

--- test/web/webquerytest.py
+++ test/web/webquerytest.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
+#
 # "Meresco Components" are components to build searchengines, repositories
-# and archives, based on "Meresco Core". 
-# 
+# and archives, based on "Meresco Core".
+#
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2015 Stichting Kennisnet http://www.kennisnet.nl
+#
 # This file is part of "Meresco Components"
-# 
+#
 # "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from unittest import TestCase
 
 from meresco.components.web import WebQuery
 from meresco.components.web.webquery import _feelsLikePlusMinusQuery, _feelsLikeBooleanQuery
-from cqlparser import parseString as parseCql
+from cqlparser import parseString as parseCql, cqlToExpression
 
 class WebQueryTest(TestCase):
 
@@ -72,13 +73,13 @@ class WebQueryTest(TestCase):
         self.assertDefaultQuery('label="value value"')
         self.assertDefaultQuery('water AND "+(rain" AND "-snow)"', 'water +(rain -snow)', needsBooleanHelp=True)
         self.assertDefaultQuery('water AND "+(rain" AND or AND "snow)"', 'water +(rain or snow)', needsBooleanHelp=True)
-        
+
     def testParseErrorsWithMinusSign(self):
-        self.assertDefaultQuery('-')    
+        self.assertDefaultQuery('-')
         self.assertDefaultQuery('- AND -', '- -')
         self.assertDefaultQuery('- AND +', '- +')
         self.assertDefaultQuery('-+-')
-        
+
         self.assertDefaultQuery('cats AND - AND dogs', 'cats - dogs')
 
     def testBooleanQuery(self):
@@ -141,6 +142,7 @@ class WebQueryTest(TestCase):
         wq = WebQuery(input, antiUnaryClause='antiunary exact true')
         self.assertEquals((boolean, plusminus, default, needsBooleanHelp), (wq.isBooleanQuery(), wq.isPlusMinusQuery(), wq.isDefaultQuery(), wq.needsBooleanHelp()))
         self.assertEquals(parseCql(expected), wq.ast)
+        self.assertEqual(cqlToExpression(expected), wq.query)
         self.assertEquals(asString, wq.asString())
         self.assertEquals(input, wq.original)
 
