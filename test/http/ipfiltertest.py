73a5fd62c9c5a4a5bd7702e31127cd7983c587e4
Johan Jonkers <johan@seecr.nl>
2016-04-01 09:41:31 +0200

    JJ: ipfilter ipv6
    Conflicts:
    	deps.txt

--- test/http/ipfiltertest.py
+++ test/http/ipfiltertest.py
@@ -29,9 +29,7 @@
 #
 ## end license ##
 
-from unittest import TestCase
-
-from seecr.test import CallTrace
+from seecr.test import CallTrace, SeecrTestCase
 from meresco.core import Observable
 from meresco.components.http import IpFilter
 
@@ -41,7 +39,7 @@ def handleRequest(*args, **kwargs):
     return
     yield
 
-class IpFilterTest(TestCase):
+class IpFilterTest(SeecrTestCase):
 
     def assertValidIp(self,  address, ipranges=[], ips=[], headers={}):
         self._assertValidIp(address, ipranges, ips, headers, passed=True)
@@ -74,6 +72,8 @@ class IpFilterTest(TestCase):
         self.assertInvalidIp('127.0.0.1', ips=['127.0.0.1'], headers={'X-Meresco-Ipfilter-Fake-Ip': '192.168.1.1'})
         self.assertValidIp('127.0.0.1', ips=['192.168.1.1'], headers={'X-Meresco-Ipfilter-Fake-Ip': '192.168.1.1'})
         self.assertInvalidIp('111.1.1.1', ips=['192.168.1.1'], headers={'X-Meresco-Ipfilter-Fake-Ip': '192.168.1.1'})
+        
+        self.assertValidIp('::1', ips=['2001:41c8:10:7b:aa:6:0:2'], headers={'X-Meresco-Ipfilter-Fake-Ip': '2001:41c8:10:7b:aa:6:0:2'})
 
     def testIpfilterFakeIpHeaderKwargsUnchanged(self):
         observer = CallTrace(methods={'handleRequest': handleRequest})
@@ -101,6 +101,7 @@ class IpFilterTest(TestCase):
         self.assertValidIp('192.168.1.0', ips=["192.168.1.0"])
         self.assertInvalidIp('192.168.1.0', ips=["192.168.1.1"])
         self.assertInvalidIp('172.16.10.1', ips=["192.168.1.0"])
+        self.assertValidIp('2001:41c8:10:7b:aa:6:0:2', ips=['2001:41c8:10:7b:aa:6:0:2'])
 
     def testZeroIPs(self):
         self.assertInvalidIp('0.0.0.0', ips=['192.168.1.1'])
@@ -108,6 +109,10 @@ class IpFilterTest(TestCase):
 
     def testInRanges(self):
         self.assertValidIp('192.168.1.0', ipranges=[('192.168.1.0', '192.168.1.255')])
+        self.assertValidIp('192.168.1.0', ipranges=['192.168.1.0/24'])
+        self.assertValidIp('192.168.1.250', ipranges=['192.168.1.240/28'])
+        self.assertValidIp('2001:41c8:10:7b:aa:6:0:2', ipranges=['2001:41c8:10:7b::/64'])
+        self.assertValidIp('2001:41c8:10:7b:aa:6:0:2', ipranges=[('2001:41c8:10:7b:aa:6:0:1', '2001:41c8:10:7b:aa:6:0:5')])
         self.assertValidIp('192.168.1.128', ipranges=[('192.168.1.0', '192.168.1.255')])
         self.assertValidIp('192.168.1.128', ipranges=[('192.168.2.0', '192.168.2.255'), ('192.168.1.0', '192.168.1.255')])
         self.assertValidIp('192.168.1.255', ipranges=[('192.168.1.0', '192.168.1.255')])
@@ -117,6 +122,8 @@ class IpFilterTest(TestCase):
         self.assertInvalidIp('192.168.1.255', ipranges=[('192.168.1.0', '192.168.1.254')])
         self.assertInvalidIp('192.168.2.0', ipranges=[('192.168.1.0', '192.168.1.255')])
         self.assertInvalidIp('192.168.0.255', ipranges=[('192.168.1.0', '192.168.1.255')])
+        self.assertInvalidIp('192.168.1.230', ipranges=['192.168.1.240/28'])
+        self.assertInvalidIp('2001:41c8:10:7a:aa:6:0:2', ipranges=['2001:41c8:10:7b::/64'])
 
     def testFilterIpAddress(self):
         ipf = IpFilter(allowedIps=['10.0.0.1'])
@@ -129,16 +136,15 @@ class IpFilterTest(TestCase):
         self.assertEquals(False, ipf.filterIpAddress(ipaddress='127.99.99.99', Headers=Headers))
 
         self.assertEquals(True, ipf.filterIpAddress(ipaddress='127.0.0.1', Headers=Headers))
+        
+        ipf = IpFilter(allowedIps=['2001:41c8:10:7b:aa:6:0:1'])
+        self.assertEquals(False, ipf.filterIpAddress(ipaddress='::1'))
+        self.assertEquals(True, ipf.filterIpAddress(ipaddress='2001:41c8:10:7b:aa:6:0:1'))
 
-    def testConvertToNumber(self):
-        iprange = IpFilter()
-
-        self.assertEquals(3232235776, iprange.convertToNumber('192.168.1.0'))
-        self.assertEquals(0, iprange.convertToNumber('0.0.0.0'))
 
     def testUpdateIpFilter(self):
         observer = CallTrace(methods={'handleRequest': handleRequest})
-        ipf = IpFilter(allowedIps=['192.168.1.1'], allowedIpRanges=[('10.0.0.1', '10.0.0.2')])
+        ipf = IpFilter(allowedIps=['192.168.1.1'], allowedIpRanges=[('10.0.0.1', '10.0.0.2'), '2001:41c8:10:7b::/64'])
 
         dna = be(
             (Observable(),
@@ -153,13 +159,21 @@ class IpFilterTest(TestCase):
         self.assertEquals(0, len(observer.calledMethods))
         list(compose(dna.all.handleRequest(Client=('192.168.1.1',), Headers={})))
         self.assertEquals(1, len(observer.calledMethods))
+        del observer.calledMethods[:]
 
+        list(compose(dna.all.handleRequest(Client=('2001:41c8:10:7b:aa:6:0:2', ), Headers={})))
+        self.assertEquals(1, len(observer.calledMethods))
         del observer.calledMethods[:]
         
-        ipf.updateIps(ipAddresses=['127.0.0.1'], ipRanges=[('10.0.0.1', '10.0.0.255')])
+        ipf.updateIps(ipAddresses=['127.0.0.1'], ipRanges=[('10.0.0.1', '10.0.0.255'), '2001:41c8:10:7c::/64'])
         list(compose(dna.all.handleRequest(Client=('192.168.1.1',), Headers={})))
         self.assertEquals(0, len(observer.calledMethods))
         list(compose(dna.all.handleRequest(Client=('127.0.0.1',), Headers={})))
         list(compose(dna.all.handleRequest(Client=('10.0.0.10',), Headers={})))
         self.assertEquals(2, len(observer.calledMethods))
+        
+        list(compose(dna.all.handleRequest(Client=('2001:41c8:10:7b:aa:6:0:2', ), Headers={})))
+        self.assertEquals(2, len(observer.calledMethods))
+        list(compose(dna.all.handleRequest(Client=('2001:41c8:10:7c:aa:6:0:2', ), Headers={})))
+        self.assertEquals(3, len(observer.calledMethods))
 
