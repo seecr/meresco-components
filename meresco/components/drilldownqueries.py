d9f5d5109fd64478fb0da9de6662e74a65112304
Hendrik Morée <hendrik@seecr.nl>
2016-06-27 14:47:51 +0200

    JJ/HM: extraArguments not required

--- meresco/components/drilldownqueries.py
+++ meresco/components/drilldownqueries.py
@@ -1,26 +1,25 @@
 ## begin license ##
 #
-# "NBC+" also known as "ZP (ZoekPlatform)" is
-#  initiated by Stichting Bibliotheek.nl to provide a new search service
-#  for all public libraries in the Netherlands.
+# "Meresco Components" are components to build searchengines, repositories
+# and archives, based on "Meresco Core".
 #
-# Copyright (C) 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014, 2016 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 #
-# This file is part of "NBC+ (Zoekplatform BNL)"
+# This file is part of "Meresco Components"
 #
-# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
+# "Meresco Components" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
+# "Meresco Components" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
+# along with "Meresco Components"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
@@ -29,9 +28,9 @@ from meresco.core import Observable
 
 
 class DrilldownQueries(Observable):
-    def executeQuery(self, extraArguments, **kwargs):
+    def executeQuery(self, extraArguments=None, **kwargs):
         drilldownQueries = []
-        xDrilldownQueries = extraArguments.get('x-drilldown-query')
+        xDrilldownQueries = extraArguments.get('x-drilldown-query') if extraArguments else None
         if xDrilldownQueries:
             for q in xDrilldownQueries:
                 if '=' in q:
